976af293a24d70b18d88844f0f2b8825be2353ae
Shaghay <102187856+Shaghay@users.noreply.github.com>
2023-03-07 20:28:43 +0100

    adding checkout form, form validation, and sending post request

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {
-    cartCtx.removeItem(id)
-  }
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = item => {
-    cartCtx.addItem({...item, amount: 1});
-  }
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true)
+  };
+
+  const submitOrderHandler = (userData) => {
+    fetch('https://food-order-253d-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
+  };
 
   const cartItems = (
-    <ul className={classes['card-items']}>
+    <ul className={classes["card-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -33,6 +49,14 @@ const Cart = (props) => {
     </ul>
   );
 
+  const ModalActions = <div className={classes.actions}>
+
+  <button className={classes["button--alt"]} onClick={props.onClose}>
+  Close</button>
+  {hasItem && (<button className={classes.button} onClick={orderHandler}>
+  Order</button>)}
+  </div>;
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -40,12 +64,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItem && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onSubmit={submitOrderHandler} onCancel={props.onClose} />}
+      {!isCheckout && ModalActions}   
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,10 +7,16 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch('https://food-order-253d-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+      const response = await fetch(
+        'https://food-order-253d-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+        );
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -25,7 +31,12 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals(); 
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    })
+     
   }, []);
   
   const mealsList = meals.map((meal) => (
@@ -42,7 +53,12 @@ const AvailableMeals = () => {
       <p>Loading...</p>
     </section>
   }
-  
+  if (httpError) {
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
+
   return (
     <section className={classes.meals}>
       <Card>
